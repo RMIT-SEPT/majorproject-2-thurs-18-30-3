072b68299a7a7011430a42f6c6c758e3d5253740
Carl Dietz <s3668498@student.rmit.edu.au>
2020-08-09 13:56:59 +1000

    React API GET protype

--- FrontEnd/src/containers/App.js
+++ FrontEnd/src/containers/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from '../res/logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import { Table, Button } from 'reactstrap';
+class App extends Component {
+	state = {
+		bookings: []
+	}
+	
+	componentDidMount() {
+		fetch('http://jsonplaceholder.typicode.com/posts').then(
+			res => res.json()).then(
+				(data) => {
+					this.setState({ bookings: data })
+				}
+			).catch(
+				console.log
+			)
+	}
+	
+	
+	render() {
+		let bookings = this.state.bookings.map((booking) => {
+			return (
+		<tr key ={booking.id}>
+						<td>{booking.id}</td>
+						<td>{booking.title}</td>
+						<td>{booking.body}</td>
+						<td>
+							<Button color = "success" size = "sm" className = "mr-2">Edit</Button>
+							<Button color = "danger" size = "sm">Delete</Button>
+						</td>
+					</tr>
+			)
+		});
+		return (
+		<div className = "app container">
+			<Table>
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>SERVICE</th>
+						<th>DESCRIPTION</th>
+						<th>ACTTION</th>
+					</tr>
+				</thead>
+				
+				<tbody>
+					{bookings}
+				</tbody>
+			</Table>
+		</div>
+		);
+	}
 }
-
 export default App;
--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './containers/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './containers/index.css';
 import App from './containers/App';
 //import * as serviceWorker from './serviceWorker';
 
