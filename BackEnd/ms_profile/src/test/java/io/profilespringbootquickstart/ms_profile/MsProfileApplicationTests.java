829f3d4bd46c5f944e76b105d037ba7e2653167c
Danny Van <s3717353@student.rmit.edu.au>
2020-08-12 21:25:52 +1000

    added ms_profile microservice to backend

---
+++ BackEnd/ms_profile/src/test/java/io/profilespringbootquickstart/ms_profile/MsProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package io.profilespringbootquickstart.ms_profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
