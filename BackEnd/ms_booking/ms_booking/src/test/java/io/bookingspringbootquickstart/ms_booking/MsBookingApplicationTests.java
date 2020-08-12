f69d4f1b2401929940f005b9809502697eb6623e
Minjin chou <49186771+kuntakinte777@users.noreply.github.com>
2020-08-13 00:36:06 +1000

    Created ms_booking microservice in backend

---
+++ BackEnd/ms_booking/ms_booking/src/test/java/io/bookingspringbootquickstart/ms_booking/MsBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bookingspringbootquickstart.ms_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
