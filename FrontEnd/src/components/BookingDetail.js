9b224afb5fc476f5ac1c2df438abcdc004b08845
north23 <49186624+north23@users.noreply.github.com>
2020-08-13 23:45:52 +1000

    Use axios library for GET request

--- FrontEnd/src/components/BookingDetail.js
+++ FrontEnd/src/components/BookingDetail.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import axios from 'axios'
 
 import '../containers/App.css';
 
@@ -12,11 +12,13 @@ function BookingDetail( { match } ) {
     const [booking, setBooking] = useState({});
 
     const fetchItem = async () => {
-        const fetchItem = await fetch(`http://localhost:3004/services/${match.params.id}`);
-
-        const booking = await fetchItem.json();
-        setBooking(booking);
-        console.log(booking);
+        try {
+            const booking = await axios.get(`http://localhost:3004/services/${match.params.id}`).data
+            setBooking(booking);
+            console.log(booking);
+        } catch (err) {
+            alert('Something went wrong' + err)
+        }
     }
 
 
