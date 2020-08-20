6e7bfa64e0dc5c22113918daa41ebc09504339f2
Danny Van <s3717353@student.rmit.edu.au>
2020-08-20 14:20:31 +1000

    added ms_profile microservice

---
+++ BackEnd/ms_profile/src/main/java/io/ms_profile/artifact_ms_profile/ArtifactMsProfileApplication.java
@@ -0,0 +1,13 @@
+package io.ms_profile.artifact_ms_profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactMsProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtifactMsProfileApplication.class, args);
+    }
+
+}
---
+++ BackEnd/ms_profile/src/test/java/io/ms_profile/artifact_ms_profile/ArtifactMsProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ms_profile.artifact_ms_profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactMsProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
