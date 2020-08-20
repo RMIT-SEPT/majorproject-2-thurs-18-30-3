4c9deb9475bd421e850cf82239afaf2d8b569d48
north23 <s3679216@student.rmit.edu.au>
2020-08-20 19:15:24 +0700

    Use Navbar from React Bootstrap

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -4,6 +4,8 @@ import {BrowserRouter} from 'react-router-dom'
 
 import App from './containers/App'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
