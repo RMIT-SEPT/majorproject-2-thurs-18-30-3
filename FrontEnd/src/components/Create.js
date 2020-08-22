33985f5b9bb5cf97ecd05d85e32cd3ada8e4efb6
north23 <s3679216@student.rmit.edu.au>
2020-08-22 21:14:45 +0700

    Finalize registration form (Material UI)

--- FrontEnd/src/components/Create.js
+++ FrontEnd/src/components/Create.js
@@ -1,163 +1,141 @@
-import React, {useState, useRef} from 'react'
-import Form from 'react-validation/build/form'
-import Input from 'react-validation/build/input'
-import CheckButton from 'react-validation/build/button'
-import {isEmail} from 'validator'
+import React, {useState} from 'react'
+import {useForm} from 'react-hook-form'
+import {yupResolver} from '@hookform/resolvers'
+import * as yup from 'yup'
+import {makeStyles} from '@material-ui/core/styles'
+import {Grid, Card, CardActions, CardContent, TextField, Button, Snackbar} from '@material-ui/core'
+import MuiAlert from '@material-ui/lab/Alert'
 
 import AuthService from '../services/auth.service'
 
-const required = (value) => {
-  if (!value) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        This field is required!
-      </div>
-    )
-  }
+const schema = yup.object().shape({
+  username: yup.string().required(),
+  password: yup.string().required().min(8, 'Must be at least 8 characters').max(19, 'Must be less than 20 characters'),
+  confirmPassword: yup
+    .string()
+    .required()
+    .min(8, 'Must be at least 8 characters')
+    .max(20, 'Must be not more than 20 characters'),
+  email: yup.string().email().required(),
+  mobile: yup
+    .string()
+    .matches(/^[0-9]+$/, 'Must be only digits')
+    .min(10, 'Must be at least 10 digits')
+    .max(12, 'Must be less than 13 digits'),
+})
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />
 }
 
-const validEmail = (value) => {
-  if (!isEmail(value)) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        This is not a valid email.
-      </div>
-    )
-  }
-}
+const Create = () => {
+  const classes = useStyles()
+  const {register, handleSubmit, errors} = useForm({
+    resolver: yupResolver(schema),
+  })
+  const [alertMsg, setAlertMsg] = useState('')
+  const [registerSuccess, setRegisterSuccess] = useState(false)
 
-const vusername = (value) => {
-  if (value.length < 3 || value.length > 20) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        The username must be between 3 and 20 characters.
-      </div>
-    )
-  }
-}
+  const onSubmit = async (data) => {
+    console.log('Registration data', data)
+    const {username, password, confirmPassword, email} = data
+    if (password !== confirmPassword) return setAlertMsg('Password mismatch')
 
-const vpassword = (value) => {
-  if (value.length < 6 || value.length > 40) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        The password must be between 6 and 40 characters.
-      </div>
-    )
-  }
-}
-
-const Create = (props) => {
-  const form = useRef()
-  const checkBtn = useRef()
-
-  const [username, setUsername] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [successful, setSuccessful] = useState(false)
-  const [message, setMessage] = useState('')
-
-  const onChangeUsername = (e) => {
-    const username = e.target.value
-    setUsername(username)
-  }
-
-  const onChangeEmail = (e) => {
-    const email = e.target.value
-    setEmail(email)
-  }
-
-  const onChangePassword = (e) => {
-    const password = e.target.value
-    setPassword(password)
-  }
-
-  const handleRegister = (e) => {
-    e.preventDefault()
-
-    setMessage('')
-    setSuccessful(false)
-
-    form.current.validateAll()
-
-    if (checkBtn.current.context._errors.length === 0) {
-      AuthService.register(username, email, password).then(
-        (response) => {
-          setMessage(response.data.message)
-          setSuccessful(true)
-        },
-        (error) => {
-          const resMessage =
-            (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
-
-          setMessage(resMessage)
-          setSuccessful(false)
-        }
-      )
+    try {
+      await AuthService.register(username, email, password)
+      setRegisterSuccess(true)
+      setAlertMsg('Successfully registered')
+    } catch (err) {
+      const resMessage = err.response?.data?.message ?? err.message
+      setAlertMsg(resMessage)
     }
   }
 
+  const handleClose = () => {
+    setAlertMsg('')
+  }
+
   return (
-    <div className="col-md-12">
-      <div className="card card-container">
-        <img src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" alt="profile-img" className="profile-img-card" />
-
-        <Form onSubmit={handleRegister} ref={form}>
-          {!successful && (
-            <div>
-              <div className="form-group">
-                <label htmlFor="username">Username</label>
-                <Input
-                  type="text"
-                  className="form-control"
-                  name="username"
-                  value={username}
-                  onChange={onChangeUsername}
-                  validations={[required, vusername]}
-                />
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <Input
-                  type="text"
-                  className="form-control"
+    <>
+      <Grid container direction="row" justify="center" alignItems="center">
+        <Card className={classes.root}>
+          <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+            <CardContent>
+              <h3>Register</h3>
+              <TextField
+                inputRef={register}
+                name="username"
+                label="Username"
+                variant="outlined"
+                error={!!errors.username}
+                helperText={errors.username?.message}
+                fullWidth
+              />
+              <div>
+                <TextField
+                  inputRef={register}
                   name="email"
-                  value={email}
-                  onChange={onChangeEmail}
-                  validations={[required, validEmail]}
+                  label="Email"
+                  variant="outlined"
+                  error={!!errors.email}
+                  helperText={errors.email?.message}
+                />
+                <TextField
+                  inputRef={register}
+                  name="mobile"
+                  label="Mobile phone"
+                  variant="outlined"
+                  error={!!errors.mobile}
+                  helperText={errors.mobile?.message}
                 />
               </div>
 
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <Input
-                  type="password"
-                  className="form-control"
-                  name="password"
-                  value={password}
-                  onChange={onChangePassword}
-                  validations={[required, vpassword]}
-                />
-              </div>
-
-              <div className="form-group">
-                <button className="btn btn-primary btn-block">Sign Up</button>
-              </div>
-            </div>
-          )}
-
-          {message && (
-            <div className="form-group">
-              <div className={successful ? 'alert alert-success' : 'alert alert-danger'} role="alert">
-                {message}
-              </div>
-            </div>
-          )}
-          <CheckButton style={{display: 'none'}} ref={checkBtn} />
-        </Form>
-      </div>
-    </div>
+              <TextField
+                inputRef={register}
+                name="password"
+                label="Password"
+                type="password"
+                variant="outlined"
+                error={!!errors.password}
+                helperText={errors.password?.message}
+                fullWidth
+              />
+              <TextField
+                inputRef={register}
+                name="confirmPassword"
+                label="Confirm password"
+                type="password"
+                variant="outlined"
+                error={!!errors.confirmPassword}
+                helperText={errors.confirmPassword?.message}
+                fullWidth
+              />
+            </CardContent>
+            <CardActions>
+              <Grid container direction="row" justify="center" alignItems="center">
+                <Button type="submit" variant="contained" color="primary">
+                  Sign In
+                </Button>
+              </Grid>
+            </CardActions>
+          </form>
+        </Card>
+      </Grid>
+      <Snackbar anchorOrigin={{vertical: 'top', horizontal: 'right'}} open={!!alertMsg}>
+        <Alert onClose={handleClose} severity={registerSuccess ? 'success' : 'error'}>
+          {alertMsg}
+        </Alert>
+      </Snackbar>
+    </>
   )
 }
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+    },
+  },
+}))
 
 export default Create
--- FrontEnd/src/services/auth.service.js
+++ FrontEnd/src/services/auth.service.js
@@ -1,13 +1,24 @@
 import axios from 'axios'
 
-const API_URL = 'http://rmit:8080/apiauth/'
+const API_URL = 'http://rmit:8080/api/auth/'
 
 const register = (username, email, password) => {
-  return axios.post(API_URL + 'signup', {
-    username,
-    email,
-    password,
-  })
+  // const {data} = axios.post(API_URL + 'signup', {
+  //   username,
+  //   email,
+  //   password,
+  // })
+
+  const data = {
+    accessToken:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+  }
+
+  if (data?.accessToken) {
+    localStorage.setItem('user', JSON.stringify(data))
+  }
+
+  return data
 }
 
 const login = async (username, password) => {
@@ -25,18 +36,6 @@ const login = async (username, password) => {
   }
 
   return data
-  /* return axios.post(API_URL + 'signin',
-      {
-      username,
-      password,
-    })
-    .then((response) => {
-      if (response.data.accessToken) {
-        localStorage.setItem('user', JSON.stringify(response.data))
-      }
-
-      return response.data
-    })*/
 }
 
 const logout = () => {
