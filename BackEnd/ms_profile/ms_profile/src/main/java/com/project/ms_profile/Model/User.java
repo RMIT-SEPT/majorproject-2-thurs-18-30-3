4cad381b352d618d0032b9d01a47c2fd4cf5f191
Danny Van <s3717353@student.rmit.edu.au>
2020-08-26 16:37:02 +1000

    Added model folder

---
+++ BackEnd/ms_profile/ms_profile/src/main/java/com/project/ms_profile/Model/User.java
@@ -0,0 +1,75 @@
+package com.project.ms_profile.Model;
+
+import javax.persistence.*;
+
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String firstname;
+    private String lastname;
+    private String phone;
+    private String email;
+    private String usertype;
+    private String password;
+
+    public User(String firstname, String lastname, String phone, String email, String usertype, String password) {
+    }
+
+    public User() {
+
+    }
+
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPhone(String phone) {
+        this.firstname = phone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUsertype(String usertype) {
+        this.usertype = usertype;
+    }
+
+    public String getUsertype() {
+        return usertype;
+    }
+
+
+
+}
