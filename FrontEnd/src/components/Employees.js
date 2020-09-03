2411a631780f7795f9a5ef64d6419320837249c4
north23 <s3679216@student.rmit.edu.au>
2020-09-03 20:55:36 +0700

    :lipstick: Show Snackbar after new record is added

--- FrontEnd/src/components/Employees.js
+++ FrontEnd/src/components/Employees.js
@@ -1,39 +1,102 @@
-import React from 'react'
-import {Table} from 'reactstrap'
+import React, {useEffect, useState} from 'react'
+import {makeStyles} from '@material-ui/core/styles'
+import MaterialTable from 'material-table'
 
 import '../containers/App.css'
-
-//Displays list of Employees
+import Snackbar from '@material-ui/core/Snackbar'
+import Alert from '@material-ui/lab/Alert'
 
 function Employees(props) {
-  //Props passed down by EmployeeListContainer
-  let employees = props.employees.map((employees) => {
-    return (
-      <tr key={employees.email}>
-        <td>{employees.email}</td>
-        <td>{employees.uname}</td>
-        <td>{employees.name}</td>
-        <td>{employees.phone}</td>
-      </tr>
-    )
-  })
+  const classes = useStyles()
+  const [columns] = useState([
+    {title: 'Username', field: 'uname'},
+    {title: 'Name', field: 'name'},
+    {title: 'Email', field: 'email'},
+    {title: 'Mobile', field: 'phone'},
+  ])
+  const [employees, setEmployees] = useState([])
+  const [alertMsg, setAlertMsg] = useState('')
+  const [alertErrorMsg, setAlertErrorMsg] = useState('')
+
+  useEffect(() => {
+    if (props.employees.length > 0) {
+      setEmployees(props.employees)
+    }
+  }, [props.employees])
+
+  async function onRowAdd(newData) {
+    // TODO: call real api
+    // try {
+    //   await axios.post('/api/customers', {name: 'sdf', mobile: 'sfdsdf'})
+    //   setEmployees([...employees, newData])
+    // } catch (err) {
+    //   setAlertErrorMsg(err.messsage)
+    // }
+    // Use setTimeout to demonstrate time taken for API POST request.
+    // Remove this line when backend guys finish implementing API handler.
+    setTimeout(() => {
+      setEmployees([...employees, newData])
+      setAlertMsg('New record added!')
+    }, 600)
+  }
+
+  async function onRowUpdate(newData, oldData) {
+    setTimeout(() => {
+      const data = [...employees]
+      data[data.indexOf(oldData)] = newData
+      setEmployees(data)
+    }, 600)
+  }
+
+  async function onRowDelete(oldData) {
+    setTimeout(() => {
+      const data = [...employees]
+      data.splice(data.indexOf(oldData), 1)
+      setEmployees(data)
+    }, 600)
+  }
 
   return (
-    <div className="bookings-list">
-      <Table>
-        <thead>
-          <tr>
-            <th>EMAIL</th>
-            <th>USERNAME</th>
-            <th>NAME</th>
-            <th>PHONE NUMBER</th>
-          </tr>
-        </thead>
-
-        <tbody>{employees}</tbody>
-      </Table>
+    <div className={classes.root}>
+      <Snackbar
+        anchorOrigin={{vertical: 'top', horizontal: 'right'}}
+        open={alertMsg !== ''}
+        onClose={() => setAlertMsg('')}
+        message="I love snacks">
+        <Alert onClose={() => setAlertMsg('')} severity="success">
+          {alertMsg}
+        </Alert>
+      </Snackbar>
+      <Snackbar
+        anchorOrigin={{vertical: 'top', horizontal: 'right'}}
+        open={alertErrorMsg !== ''}
+        onClose={() => setAlertMsg('')}
+        message="I love snacks">
+        <Alert onClose={() => setAlertErrorMsg('')} severity="error">
+          {alertErrorMsg}
+        </Alert>
+      </Snackbar>
+      <MaterialTable
+        title="Employees"
+        columns={columns}
+        data={employees}
+        editable={{
+          onRowAdd,
+          onRowUpdate,
+          onRowDelete,
+        }}
+      />
     </div>
   )
 }
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(5),
+  },
+  table: {
+    minWidth: 650,
+  },
+}))
 
 export default Employees
