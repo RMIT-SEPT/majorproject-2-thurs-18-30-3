94732605a4172ff1882b1bdb99157921b425d550
north23 <s3679216@student.rmit.edu.au>
2020-09-14 21:49:38 +0700

    Move React states and API calls to EmployeeListContainer

--- FrontEnd/src/components/Employees.js
+++ FrontEnd/src/components/Employees.js
@@ -1,13 +1,13 @@
-import React, {useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import {makeStyles} from '@material-ui/core/styles'
 import Snackbar from '@material-ui/core/Snackbar'
 import Alert from '@material-ui/lab/Alert'
 import MaterialTable from 'material-table'
-import Axios from 'axios'
 
 import '../containers/App.css'
 
-function Employees(props) {
+// Many props will be deleted after code refactoring
+function Employees({employees, onRowAdd, onRowUpdate, onRowDelete, alertMsg, alertErrorMsg, onAlertClose}) {
   const classes = useStyles()
   const [columns] = useState([
     {title: 'Username', field: 'uname'},
@@ -15,69 +15,12 @@ function Employees(props) {
     {title: 'Email', field: 'email'},
     {title: 'Mobile', field: 'mobile'},
   ])
-  const [employees, setEmployees] = useState([])
-  const [alertMsg, setAlertMsg] = useState('')
-  const [alertErrorMsg, setAlertErrorMsg] = useState('')
-
-  useEffect(() => {
-    if (props.employees.length > 0) {
-      setEmployees(props.employees)
-    }
-  }, [props.employees])
-
-  async function onRowAdd(newData) {
-    // TODO: call real api
-    try {
-      await Axios.post('https://5f50f63c5e98480016123379.mockapi.io/employees', newData)
-      setEmployees([...employees, newData])
-      setAlertMsg(`${newData.name} has been added`)
-    } catch ({messsage}) {
-      setAlertErrorMsg(messsage)
-    }
-  }
-
-  async function onRowUpdate(newData, oldData) {
-    try {
-      await Axios.put(`https://5f50f63c5e98480016123379.mockapi.io/employees/${oldData.id}`, newData)
-      setEmployees([...employees, newData])
-    } catch ({messsage}) {
-      setAlertErrorMsg(messsage)
-    }
-
-    setTimeout(() => {
-      const data = [...employees]
-      data[data.indexOf(oldData)] = newData
-      setEmployees(data)
-    }, 600)
-  }
-
-  async function onRowDelete(oldData) {
-    try {
-      await Axios.delete(`https://5f50f63c5e98480016123379.mockapi.io/employees/${oldData.id}`)
-      const data = [...employees]
-      data.splice(data.indexOf(oldData), 1)
-      setEmployees(data)
-    } catch ({messsage}) {
-      setAlertErrorMsg(messsage)
-    }
-  }
 
   return (
     <div className={classes.root}>
-      <Snackbar
-        anchorOrigin={{vertical: 'top', horizontal: 'right'}}
-        open={alertMsg !== ''}
-        onClose={() => setAlertMsg('')}>
-        <Alert onClose={() => setAlertMsg('')} severity="success">
-          {alertMsg}
-        </Alert>
-      </Snackbar>
-      <Snackbar
-        anchorOrigin={{vertical: 'top', horizontal: 'right'}}
-        open={alertErrorMsg !== ''}
-        onClose={() => setAlertErrorMsg('')}>
-        <Alert onClose={() => setAlertErrorMsg('')} severity="error">
-          {alertErrorMsg}
+      <Snackbar anchorOrigin={{vertical: 'top', horizontal: 'right'}} open={alertMsg !== ''} onClose={onAlertClose}>
+        <Alert onClose={onAlertClose} severity={alertMsg ? 'success' : 'error'}>
+          {alertMsg ?? alertErrorMsg}
         </Alert>
       </Snackbar>
       <MaterialTable
--- FrontEnd/src/containers/EmployeeListContainer.js
+++ FrontEnd/src/containers/EmployeeListContainer.js
@@ -3,13 +3,15 @@ import Axios from 'axios'
 import Employees from '../components/Employees'
 
 function EmployeeListContainer() {
-  const [employees, setEmployess] = useState([])
+  const [employees, setEmployees] = useState([])
+  const [alertMsg, setAlertMsg] = useState('')
+  const [alertErrorMsg, setAlertErrorMsg] = useState('')
 
   useEffect(() => {
     async function callAPI() {
       try {
         const {data} = await Axios.get('https://5f50f63c5e98480016123379.mockapi.io/employees')
-        setEmployess(data)
+        setEmployees(data)
       } catch (err) {
         alert(err.message)
       }
@@ -17,7 +19,59 @@ function EmployeeListContainer() {
     callAPI()
   }, [])
 
-  return <Employees employees={employees} />
+  function onAlertClose() {
+    setAlertMsg('')
+    setAlertErrorMsg('')
+  }
+
+  async function onRowAdd(newData) {
+    // TODO: call real api
+    try {
+      await Axios.post('https://5f50f63c5e98480016123379.mockapi.io/employees', newData)
+      setEmployees([...employees, newData])
+      setAlertMsg(`${newData.name} has been added`)
+    } catch ({messsage}) {
+      setAlertErrorMsg(messsage)
+    }
+  }
+
+  async function onRowUpdate(newData, oldData) {
+    try {
+      await Axios.put(`https://5f50f63c5e98480016123379.mockapi.io/employees/${oldData.id}`, newData)
+      setEmployees([...employees, newData])
+    } catch ({messsage}) {
+      setAlertErrorMsg(messsage)
+    }
+
+    setTimeout(() => {
+      const data = [...employees]
+      data[data.indexOf(oldData)] = newData
+      setEmployees(data)
+    }, 600)
+  }
+
+  async function onRowDelete(oldData) {
+    try {
+      await Axios.delete(`https://5f50f63c5e98480016123379.mockapi.io/employees/${oldData.id}`)
+      const data = [...employees]
+      data.splice(data.indexOf(oldData), 1)
+      setEmployees(data)
+    } catch ({messsage}) {
+      setAlertErrorMsg(messsage)
+    }
+  }
+
+  return (
+    <Employees
+      employees={employees}
+      onRowAdd={onRowAdd}
+      onRowUpdate={onRowUpdate}
+      onRowDelete={onRowDelete}
+      alertMsg={alertMsg}
+      alertErrorMsg={alertErrorMsg}
+      onAlertClose={onAlertClose}
+    />
+  )
 }
 
 export default EmployeeListContainer
