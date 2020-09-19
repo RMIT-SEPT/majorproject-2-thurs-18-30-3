21d121201a65273f8b8ad62c6ad3f9f311d03765
Minjin chou <49186771+kuntakinte777@users.noreply.github.com>
2020-09-19 13:26:04 +1000

    Revert "unit tests"

--- BackEnd/ms_profiles/src/test/java/io/microservices/ms_profiles/MsProfilesApplicationTests.java
+++ BackEnd/ms_profiles/src/test/java/io/microservices/ms_profiles/MsProfilesApplicationTests.java
@@ -1,29 +1,13 @@
 package io.microservices.ms_profiles;
 
-import io.microservices.ms_profiles.exceptions.ProfilesException;
-import io.microservices.ms_profiles.model.Profiles;
-import io.microservices.ms_profiles.repositories.ProfilesRepository;
-import io.microservices.ms_profiles.services.ProfileService;
-import org.junit.Assert;
-import org.junit.Rule;
 import org.junit.jupiter.api.Test;
-import org.junit.rules.ExpectedException;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MsProfilesApplicationTests {
-    private ProfileService p;
-
-    @Rule
-    public ExpectedException exceptionRule = ExpectedException.none();
 
     @Test
-    void testFindUsernameNullExceptions() {
-        try {
-            p.findByUsername("");
-        } catch (Exception e) {
-            exceptionRule.expect(NullPointerException.class);
-            exceptionRule.expectMessage("Null username.");
-        }
+    void contextLoads() {
     }
+
 }
