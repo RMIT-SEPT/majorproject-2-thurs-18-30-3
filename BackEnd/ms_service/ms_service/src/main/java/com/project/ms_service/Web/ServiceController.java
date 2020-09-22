5630c72e341f8b0014dd97ef178907c2faf2a304
Minjin chou <49186771+kuntakinte777@users.noreply.github.com>
2020-09-22 22:54:54 +1000

    Update ServiceController.java

--- BackEnd/ms_service/ms_service/src/main/java/com/project/ms_service/Web/ServiceController.java
+++ BackEnd/ms_service/ms_service/src/main/java/com/project/ms_service/Web/ServiceController.java
@@ -13,6 +13,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/services")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ServiceController {
 
     @Autowired
