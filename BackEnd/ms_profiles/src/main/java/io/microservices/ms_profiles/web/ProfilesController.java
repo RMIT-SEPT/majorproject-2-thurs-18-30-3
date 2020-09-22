62f0fe986ba42914a77a9a5f09777d03addeae8e
north23 <s3679216@student.rmit.edu.au>
2020-09-23 00:13:15 +0700

    :bulb: Uncomment needed code

--- BackEnd/ms_profiles/src/main/java/io/microservices/ms_profiles/web/ProfilesController.java
+++ BackEnd/ms_profiles/src/main/java/io/microservices/ms_profiles/web/ProfilesController.java
@@ -20,7 +20,7 @@ public class ProfilesController {
     @Autowired
     private ProfileService profileService;
 
-    //@Autowired
+    @Autowired
     private MapValidationErrorService mapValidation;
 
     // Controller for POST request.
