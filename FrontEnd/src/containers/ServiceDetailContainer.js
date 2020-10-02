7faab40a3acca6e7823f01c78192a39a9a932135
north23 <s3679216@student.rmit.edu.au>
2020-10-02 20:57:06 +0700

    :recycle: Convert to React Hooks

--- FrontEnd/src/containers/ServiceDetailContainer.js
+++ FrontEnd/src/containers/ServiceDetailContainer.js
@@ -1,35 +1,28 @@
-import React, {  } from 'react';
-import serviceApi from "../config/serviceApi";
-const { default: ServiceDetail } = require("../components/ServiceDetail");
+import React, {useState, useEffect} from 'react'
+import Axios from 'axios'
+import {useParams} from 'react-router-dom'
+import serviceApi from '../config/serviceApi'
+const {default: ServiceDetail} = require('../components/ServiceDetail')
 
 //Retrieves data for detailed display of a single service
-class ServiceDetailContainer extends React.Component {
+function ServiceDetailContainer() {
+  const {id} = useParams()
+  const [service, setService] = useState({})
 
-    constructor(props){
-        super(props);
-        this.state = {
-            service: {}
-        }
+  useEffect(() => {
+    const callApi = async () => {
+      try {
+        const {data} = await Axios.get(serviceApi.getService(id))
+        setService(data)
+      } catch ({message}) {
+        console.error('[ServiceDetailContainer]', message)
+        alert(`Cannot get service with ID ${id}: ${message}`)
+      }
     }
+    callApi()
+  }, [id])
 
-  //GET data request from API
-  /*Forms an api request from the url passed to the page*/
-  componentDidMount() {
-    const { id } = this.props.match.params;
-    const url = serviceApi.getService(id);
-    
-	  fetch(url).then(
-      res => res.json()).then(
-        (data) => {
-          this.setState({ service: data })
-        }
-      ).catch(
-        console.log
-      )
-  }
-    
-    render() {
-      return <ServiceDetail service={this.state.service} />;
-    }
-  }
-  export default ServiceDetailContainer;
+  return <ServiceDetail service={service} />
+}
+
+export default ServiceDetailContainer
