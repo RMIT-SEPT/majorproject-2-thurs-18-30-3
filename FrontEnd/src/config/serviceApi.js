9733e2f1f02d5cfdc524c758d17be72fd995d3c1
rmit-s3668498-Carl-Dietz <30914292+rmit-s3668498-Carl-Dietz@users.noreply.github.com>
2020-10-10 14:47:46 +1100

    Update serviceApi.js
    updated service api for integration

--- FrontEnd/src/config/serviceApi.js
+++ FrontEnd/src/config/serviceApi.js
@@ -1,7 +1,7 @@
-// const BASE_URL = http://localhost:8080/api/
-const BASE_URL = 'https://5f51c3975e98480016123e31.mockapi.io/services'
+const BASE_URL = http://localhost:8080/api/
+//const BASE_URL = 'https://5f51c3975e98480016123e31.mockapi.io/services'
 
 export default {
-  getAllServices: BASE_URL,
+  getAllServices: `${BASE_URL}/all`,
   getService: (id) => `${BASE_URL}/${id}`,
 }
