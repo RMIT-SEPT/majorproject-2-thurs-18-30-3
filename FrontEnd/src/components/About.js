fc6b2dfd3b3ad6131b63a357b2be8d1a6dc763e0
s3668498 <s3668498@student.rmit.edu.au.com>
2020-10-15 19:28:07 +1100

    about page, booking delete fix

--- FrontEnd/src/components/About.js
+++ FrontEnd/src/components/About.js
@@ -7,6 +7,21 @@ function About() {
     return (
         <div className="container">
             <h1>About</h1>
+            <p>
+            Our story begins in Late Autumn 1884, when gentleman pastoralist Emery Orphee enlisted Dr Diomira Ku to orgniase a series of java developers in an effort 
+                to raise a trading company on the shores of the Pensacola. Legal operations were initialised using IB-LotusNotes on March 21, however would not be completed until March 31, 
+                when the typeface of the SRS was changed from .42 GILROY to .17 Neue Grotesk. This precipitated the grand opening, where the studio was declared as 'ἄγμε πηδαλιούχος' to a 
+                crowd of 101 people. During the winter months the running of AGME was peformed out of a 10x5 shipping container left from the wreck of the Palatka. At first, Ku was totally against 
+                the idea of customer/consumer relations and a de Maurier Novel was used to emulate human interfacing, by means of feeding pages through a shredder to produce fertiliser. Following the conclusion of 
+                the winter sprint storage space was exapanded into a second, more oblong container, 8x8, thus paving the way for serious html operations and for the first time, the opening of frontend 
+                seeding. In 1892 procurement of CSS theatrics massively juiced productivity to tremendous accolade in the release of that same year. Yet, the situation changed in the form of 
+                'banking crisis' and all gitflow was halted. Conesequently Dr. Ku was unable to complete the spinting procedure that had been expected, forcing the developers into a cycle of 
+                crunch/burnout to resuscitate the ailing Orphee (who had at this point been attached by means of calcium tubing to serve as an emergent organic POM). This would be the 
+                direct cause of the fire scouring the wreck of the Palatka and AGME's flight to Victoria BC. However this occasion would prove joyous as in spite of second degree burning, 
+                SPRINTFOUR was met with applaud, owing to the valiant effort of spiders contracted to manage the trello board. Such an unexpected development paid in full the Jet Fuel 
+                debt and allowed for the formal refactoring of company grounds into the campus of the old GAS&FUEL corporation buildings. Such magnificent slabs of business space would 
+                nourish and nurse the ailing Orphee root branch and stem.
+                </p>
         </div>
     );
 }
